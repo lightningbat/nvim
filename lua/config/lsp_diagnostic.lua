fbb0dc4c5ee05f39f64bbef299130bb1a966e6f3
lightningbat <183808230+lightningbat@users.noreply.github.com>
2026-04-03 15:47:26 +0530

    fix(lsp): allow diagnostic floating windows to take focus

--- lua/config/lsp_diagnostic.lua
+++ lua/config/lsp_diagnostic.lua
@@ -21,7 +21,7 @@ vim.o.updatetime = 250 -- faster hover updates
 vim.api.nvim_create_autocmd("CursorHold", {
 	callback = function()
 		vim.diagnostic.open_float(nil, {
-			focusable = false,
+			focusable = true,
 			scope = "cursor",
 			border = "rounded",
 		})
